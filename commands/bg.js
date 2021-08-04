a917f5cac9ef08211c16e3a6af1168040bb21734
Connor Ahern <dck.dachickenking@gmail.com>
2021-08-04 12:59:52 -0700

    Add the ability to remove card background

--- commands/bg.js
+++ commands/bg.js
@@ -11,7 +11,10 @@ module.exports = {
     cooldown: 3,
     category: "Leveling",
     async execute (message, args) {
-		if(!args[0]) return message.reply("You can use `!rank` to view your current rank card background!");
+		if(!args[0]) {
+			sql.prepare("DELETE FROM background WHERE user = (?);").run(message.author.id);
+			return message.reply("Your rank card background has been reset!");
+		}
         var valid = /^(http|https):\/\/[^ "]+$/.test(args[0]) && (message.content.endsWith(".png") || message.content.endsWith(".jpg") || message.content.endsWith("jpeg"));
 		if(!valid) return message.reply("Please enter a valid image URL. It must use HTTP or HTTPS and be a PNG, JPG, or JPEG image. It must not have a `?` or `&` at the end.");
 		
