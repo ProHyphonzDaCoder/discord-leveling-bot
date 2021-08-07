03b0c1ced4a30f7dbeabb9dc1ef8c92bd36a6885
Connor Ahern <nameisa@prodigymathwiki.com>
2021-08-07 10:00:19 -0700

    Upgrading to Discord.JS v13.0.0

--- commands/remove-level.js
+++ commands/remove-level.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const SQlite = require("better-sqlite3");
 const sql = new SQlite('./mainDB.sqlite');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
 
 module.exports = {
     name: 'remove-level',
