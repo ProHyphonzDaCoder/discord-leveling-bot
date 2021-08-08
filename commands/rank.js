744a46b03110b594c3e5054e6dd755a47e31fc0d
Connor Ahern <dck.dachickenking@gmail.com>
2021-08-08 07:15:44 -0700

    m

--- commands/rank.js
+++ commands/rank.js
@@ -7,14 +7,13 @@ const canvacord = require("canvacord");
 module.exports = {
     name: 'rank',
     aliases: ['rank'],
-    description: "Check users rank and xp",
+    description: "Check a user's rank and XP",
     cooldown: 3,
     category: "Leveling",
-    async execute(message, args) {
+    async execute(interaction) {
+        if(!interaction.isCommand()) return;
 
-        let userArray = message.content.split(" ");
-        let userArgs = userArray.slice(1);
-        let user = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
+        let user = interaction.user;
 
         client.getScore = sql.prepare("SELECT * FROM levels WHERE user = ? AND guild = ?");
         client.setScore = sql.prepare("INSERT OR REPLACE INTO levels (id, user, guild, xp, level, totalXP) VALUES (@id, @user, @guild, @xp, @level, @totalXP);");
