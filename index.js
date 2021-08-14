3fb018762fb730704a40b8dcd806741bbcdb83df
Connor Ahern <dck.dachickenking@gmail.com>
2021-08-14 11:02:57 -0700

    v1.0.0

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ const {
 const client = new Discord.Client({
   intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_PRESENCES],
 });
+const { readdirSync } = require("fs");
+
 client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
 const talkedRecently = new Map();
