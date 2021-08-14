d7e96f8224b0c0ddf9860e6a6550006b3f450141
Connor Ahern <dck.dachickenking@gmail.com>
2021-08-14 11:31:08 -0700

    v1.0.0

--- commands/bg.js
+++ commands/bg.js
@@ -6,20 +6,28 @@ module.exports = {
     aliases: ['bg'],
     description: "Set a card background",
     cooldown: 3,
+	options: [
+		{
+			name: 'bgurl',
+			description: 'The image URL for the background you wish to add (leave empty to reset)',
+			type: 3,
+            required: false
+		},
+	],
     category: "Leveling",
-    async execute (message, args) {
-		if(!args[0]) {
+    async execute (interaction) {
+		if(!interaction.options.getString("bgurl")) {
 			sql.prepare("DELETE FROM background WHERE user = (?);").run(message.author.id);
-			return message.reply("Your rank card background has been reset!");
+			return interaction.reply("Your rank card background has been reset!");
 		}
-        var valid = /^(http|https):\/\/[^ "]+$/.test(args[0]) && (message.content.endsWith(".png") || message.content.endsWith(".jpg") || message.content.endsWith("jpeg"));
-		if(!valid) return message.reply("Please enter a valid image URL. It must use HTTP or HTTPS and be a PNG, JPG, or JPEG image. It must not have a `?` or `&` at the end.");
+        var valid = /^(http|https):\/\/[^ "]+$/.test(interaction.options.getString("bgurl")) && (interaction.options.getString("bgurl").endsWith(".png") || interaction.options.getString("bgurl").endsWith(".jpg") || interaction.options.getString("bgurl").endsWith("jpeg"));
+		if(!valid) return interaction.reply("Please enter a valid image URL. It must use HTTP or HTTPS and be a PNG, JPG, or JPEG image. It must not have a `?` or `&` at the end.");
 		
 		try {
-			sql.prepare("INSERT OR REPLACE INTO background (user, guild, bg) VALUES (?, ?, ?);").run(message.author.id, message.guild.id, args[0]);
-			message.reply("Background set!");
+			sql.prepare("INSERT OR REPLACE INTO background (user, guild, bg) VALUES (?, ?, ?);").run(interaction.member.id, interaction.guild.id, interaction.options.getString("bgurl"));
+			interaction.reply("Background set!");
 		} catch (e) {
-			message.reply("An error occurred while setting the background.");
+			interaction.reply("An error occurred while setting the background.");
 			console.error(e);
 		}
     }
