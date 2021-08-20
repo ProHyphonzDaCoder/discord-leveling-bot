4d20a8cf6c99f36c69c79ad29fb29e1dbbe12808
Connor Ahern <dck.dachickenking@gmail.com>
2021-08-19 21:39:50 -0700

    Attempting to fix #28

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const client = new Discord.Client({
     intents: [
         Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
         Discord.Intents.FLAGS.GUILD_PRESENCES
     ]
 });
