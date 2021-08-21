a02046542e48a03512ef9fe4a0d01899218e550b
Connor Ahern <dck.dachickenking@gmail.com>
2021-08-21 13:42:37 -0700

    Fixed a  bg.js bug & beautified

--- commands/bg.js
+++ commands/bg.js
@@ -6,30 +6,28 @@ module.exports = {
     aliases: ['bg'],
     description: "Set a card background",
     cooldown: 3,
-	options: [
-		{
-			name: 'bgurl',
-			description: 'The image URL for the background you wish to add (leave empty to reset)',
-			type: 3,
-            required: false
-		},
-	],
+    options: [{
+        name: 'bgurl',
+        description: 'The image URL for the background you wish to add (leave empty to reset)',
+        type: 3,
+        required: false
+    }, ],
     category: "Leveling",
-    async execute (interaction) {
-	    await interaction.deferReply();
-		if(!interaction.options.getString("bgurl")) {
-			sql.prepare("DELETE FROM background WHERE user = (?);").run(interaction.user.id);
-			return await interaction.editReply("Your rank card background has been reset!");
-		}
+    async execute(interaction) {
+        await interaction.deferReply();
+        if (!interaction.options.getString("bgurl")) {
+            sql.prepare("DELETE FROM background WHERE user = (?);").run(interaction.user.id);
+            return await interaction.editReply("Your rank card background has been reset!");
+        }
         var valid = /^(http|https):\/\/[^ "]+$/.test(interaction.options.getString("bgurl")) && (interaction.options.getString("bgurl").endsWith(".png") || interaction.options.getString("bgurl").endsWith(".jpg") || interaction.options.getString("bgurl").endsWith("jpeg"));
-		if(!valid) return interaction.reply("Please enter a valid image URL. It must use HTTP or HTTPS and be a PNG, JPG, or JPEG image. It must not have a `?` or `&` at the end.");
-		
-		try {
-			sql.prepare("INSERT OR REPLACE INTO background (user, guild, bg) VALUES (?, ?, ?);").run(interaction.member.id, interaction.guild.id, interaction.options.getString("bgurl"));
-			await interaction.editReply("Background set!");
-		} catch (e) {
-			await interaction.editReply("An error occurred while setting the background.");
-			console.error(e);
-		}
+        if (!valid) return await interaction.editReply("Please enter a valid image URL. It must use HTTP or HTTPS and be a PNG, JPG, or JPEG image. It must not have a `?` or `&` at the end.");
+
+        try {
+            sql.prepare("INSERT OR REPLACE INTO background (user, guild, bg) VALUES (?, ?, ?);").run(interaction.member.id, interaction.guild.id, interaction.options.getString("bgurl"));
+            return await interaction.editReply("Background set!");
+        } catch (e) {
+            return await interaction.editReply("An error occurred while setting the background.");
+            console.error(e);
+        }
     }
 }
