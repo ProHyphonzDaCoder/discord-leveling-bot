cdc76a827f507859fffea6f35f1535b4cac01d29
m8eee <aldous0601@yahoo.com>
2021-08-23 07:48:53 +0800

    Less Queries During Setup and Removal of Prefix

--- sql_functions/sql_functions.js
+++ sql_functions/sql_functions.js
@@ -1,51 +1,14 @@
 const SQLite = require("better-sqlite3");
 const sql = new SQLite('./mainDB.sqlite');
 
-const levelTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'levels';").get();
-if (!levelTable['count(*)']) {
-    sql.prepare("CREATE TABLE levels (id TEXT PRIMARY KEY, user TEXT, guild TEXT, xp INTEGER, level INTEGER, totalXP INTEGER);").run();
-}
-
-// Check if the table "backgrounds" exists.
-const bgTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'background';").get();
-if (!bgTable['count(*)']) {
-    sql.prepare("CREATE TABLE background (user TEXT, guild TEXT, bg TEXT)").run();
-}
-
-// Role table for levels
-const roleTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'roles';").get();
-if (!roleTable['count(*)']) {
-    sql.prepare("CREATE TABLE roles (guildID TEXT, roleID TEXT, level INTEGER);").run();
-}
-
-// Prefix table
-const prefixTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'prefix';").get();
-if (!prefixTable['count(*)']) {
-    sql.prepare("CREATE TABLE prefix (serverprefix TEXT, guild TEXT PRIMARY KEY);").run();
-}
-
-// Blacklist table
-const blacklistTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'blacklistTable';").get();
-if (!blacklistTable['count(*)']) {
-    sql.prepare("CREATE TABLE blacklistTable (guild TEXT, typeId TEXT, type TEXT, id TEXT PRIMARY KEY);").run();
-}
-
-// 2X XP table
-const doubleXPTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'doubleXP';").get();
-if (!doubleXPTable['count(*)']) {
-    sql.prepare("CREATE TABLE doubleXP (guild TEXT, role TEXT);").run();
-}
-
-// Settings table
-const settingsTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'settings';").get();
-if (!settingsTable['count(*)']) {
-    sql.prepare("CREATE TABLE settings (guild TEXT PRIMARY KEY, levelUpMessage TEXT, customXP INTEGER, customCooldown INTEGER);").run();
-}
-
-const channelTable = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'channel';").get();
-if (!channelTable['count(*)']) {
-    sql.prepare("CREATE TABLE channel (guild TEXT PRIMARY KEY, channel TEXT);").run();
-}
+const allTables = sql.prepare("SELECT * FROM sqlite_master WHERE type='table'").all();
+if (!allTables.find(table => table.name == "levels")) sql.exec("CREATE TABLE levels (id TEXT PRIMARY KEY, user TEXT, guild TEXT, xp INTEGER, level INTEGER, totalXP INTEGER);");
+if (!allTables.find(table => table.name == "background")) sql.exec("CREATE TABLE background (user TEXT, guild TEXT, bg TEXT)");
+if (!allTables.find(table => table.name == "roles")) sql.exec("CREATE TABLE roles (guildID TEXT, roleID TEXT, level INTEGER);");
+if (!allTables.find(table => table.name == "blacklistTable")) sql.exec("CREATE TABLE blacklistTable (guild TEXT, typeId TEXT, type TEXT, id TEXT PRIMARY KEY);");
+if (!allTables.find(table => table.name == "doubleXP")) sql.exec("CREATE TABLE doubleXP (guild TEXT, role TEXT);");
+if (!allTables.find(table => table.name == "settings")) sql.exec("CREATE TABLE settings (guild TEXT PRIMARY KEY, levelUpMessage TEXT, customXP INTEGER, customCooldown INTEGER);");
+if (!allTables.find(table => table.name == "channel")) sql.exec("CREATE TABLE channel (guild TEXT PRIMARY KEY, channel TEXT);");
 
 let getLevel = sql.prepare("SELECT * FROM levels WHERE user = ? AND guild = ?");
 let setLevel = sql.prepare("INSERT OR REPLACE INTO levels (id, user, guild, xp, level, totalXP) VALUES (?, ?, ?, ?, ?, ?);");
