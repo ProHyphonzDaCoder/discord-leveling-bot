8dfabdba4f956f445cf25dcf532091be55171064
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-24 20:08:50 +0200

    Fix(Rank): rank member presence

--- commands/rank.js
+++ commands/rank.js
@@ -1,87 +1,85 @@
-const Discord = require("discord.js");
+const { MessageAttachment } = require("discord.js");
 const SQLite = require("better-sqlite3");
-const sql = new SQLite('./mainDB.sqlite')
-const canvacord = require("canvacord");
+const sql = new SQLite("./mainDB.sqlite");
+const { Rank } = require("canvacord");
 
 module.exports = {
-    name: 'rank',
-    aliases: ['rank'],
-    description: "Get your rank or another member's rank",
-    cooldown: 3,
-    options: [{
-        name: 'target',
-        description: 'The user\'s rank card to show',
-        type: 6,
-        required: false
-    }, ],
-    category: "Leveling",
-    async execute(interaction) {
-        if (!interaction.isCommand()) return;
+	name: "rank",
+	aliases: ["rank"],
+	description: "Get your rank or another member's rank",
+	cooldown: 3,
+	options: [
+		{
+			name: "target",
+			description: "The user's rank card to show",
+			type: 6,
+			required: false,
+		},
+	],
+	category: "Leveling",
+	async execute(interaction) {
+		if (!interaction.isCommand()) return;
+		await interaction.deferReply();
 
-        await interaction.deferReply();
+		const client = interaction.client;
+		const user = interaction.options.getMember("target") || interaction.member;
 
-        const client = interaction.client;
+		client.getScore = sql.prepare("SELECT * FROM levels WHERE user = ? AND guild = ?");
+		client.setScore = sql.prepare(
+			"INSERT OR REPLACE INTO levels (id, user, guild, xp, level, totalXP) VALUES (@id, @user, @guild, @xp, @level, @totalXP);"
+		);
 
-        let user = interaction.options.getUser("target") || interaction.user;
+		const top10 = sql
+			.prepare("SELECT * FROM levels WHERE guild = ? ORDER BY totalXP")
+			.all(interaction.guild.id);
+		const score = client.getScore.get(user.id, interaction.guild.id);
 
-        client.getScore = sql.prepare("SELECT * FROM levels WHERE user = ? AND guild = ?");
-        client.setScore = sql.prepare("INSERT OR REPLACE INTO levels (id, user, guild, xp, level, totalXP) VALUES (@id, @user, @guild, @xp, @level, @totalXP);");
+		if (!score)
+			return interaction.followUp(
+				user === interaction.member
+					? "You do not have any XP yet! Chat and be active to get more XP."
+					: `${user.username} does not have any XP yet!`
+			);
 
-        const top10 = sql.prepare("SELECT * FROM levels WHERE guild = ? ORDER BY totalXP").all(interaction.guild.id);
-        let score = client.getScore.get(user.id, interaction.guild.id);
+		const levelInfo = score.level;
+		const nextXP = levelInfo * 2 * 250 + 250;
+		const xpInfo = score.xp;
+		const totalXP = score.totalXP;
 
-        if (!score) {
-            if (user == interaction.user) {
-                return interaction.editReply("You do not have any XP yet! Chat and be active to get more XP.")
-            } else {
-                return interaction.editReply(`${user.username} does not have any XP yet!`)
-            }
-        }
+		const rank = top10.sort((a, b) => b.totalXP - a.totalXP);
+		const ranking = rank.map((x) => x.totalXP).indexOf(totalXP) + 1;
 
-        const levelInfo = score.level;
-        const nextXP = levelInfo * 2 * 250 + 250;
-        const xpInfo = score.xp;
-        const totalXP = score.totalXP;
+		try {
+			var cardBg = sql
+				.prepare("SELECT bg FROM background WHERE user = ? AND guild = ?")
+				.get(user.id, interaction.guild.id).bg;
+			var bgType = "IMAGE";
+		} catch (e) {
+			var cardBg = "#000000";
+			var bgType = "COLOR";
+		}
 
-        let rank = top10.sort((a, b) => {
-            return b.totalXP - a.totalXP
-        });
+		const rankCard = new Rank()
+			.setAvatar(
+				user.user.displayAvatarURL({
+					format: "jpg",
+				})
+			)
+			.setStatus(user?.presence?.status ?? "offline", true, 1)
+			.setCurrentXP(xpInfo)
+			.setRequiredXP(nextXP)
+			.setProgressBar("#5AC0DE", "COLOR")
+			.setUsername(user.user.username)
+			.setDiscriminator(user.user.discriminator)
+			.setRank(ranking)
+			.setLevel(levelInfo)
+			.setLevelColor("#5AC0DE")
+			.renderEmojis(true)
+			.setBackground(bgType, cardBg);
 
-        let ranking = rank.map(x => x.totalXP).indexOf(totalXP) + 1
-
-        try {
-            var cardBg = sql.prepare("SELECT bg FROM background WHERE user = ? AND guild = ?").get(user.id, interaction.guild.id).bg;
-            var bgType = "IMAGE";
-        } catch (e) {
-            var cardBg = "#000000";
-            var bgType = "COLOR";
-        }
-
-        console.log(interaction.member.presence);
-        const rankCard = new canvacord.Rank()
-            .setAvatar(user.displayAvatarURL({
-                format: "jpg"
-            }))
-            .setStatus(interaction.member?.presence?.status ?? "offline", true, 1)
-            .setCurrentXP(xpInfo)
-            .setRequiredXP(nextXP)
-            .setProgressBar("#5AC0DE", "COLOR")
-            .setUsername(user.username)
-            .setDiscriminator(user.discriminator)
-            .setRank(ranking)
-            .setLevel(levelInfo)
-            .setLevelColor("#5AC0DE")
-            .renderEmojis(true)
-            .setBackground(bgType, cardBg);
-
-        rankCard.build()
-            .then(data => {
-                const attachment = new Discord.MessageAttachment(data, "RankCard.png");
-                return interaction.editReply({
-                    files: [attachment]
-                });
-            });
-
-    }
-
-}
+		const card = await rankCard.build();
+		await interaction.followUp({
+			files: [new MessageAttachment(card, "RankCard.png")],
+		});
+	},
+};
