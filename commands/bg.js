791ea1115af3a92eec39ee8eb51d38ff57acba51
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-24 20:33:09 +0200

    Fixing commands

--- commands/bg.js
+++ commands/bg.js
@@ -1,33 +1,44 @@
 const SQLite = require("better-sqlite3");
-const sql = new SQLite('./mainDB.sqlite')
+const sql = new SQLite("../mainDB.sqlite");
 
 module.exports = {
-    name: 'background',
-    aliases: ['bg'],
-    description: "Set a card background",
-    cooldown: 3,
-    options: [{
-        name: 'bgurl',
-        description: 'The image URL for the background you wish to add (leave empty to reset)',
-        type: 3,
-        required: false
-    }],
-    category: "Leveling",
-    async execute(interaction) {
-        await interaction.deferReply();
-        if (!interaction.options.getString("bgurl")) {
-            sql.prepare("DELETE FROM background WHERE user = (?);").run(interaction.user.id);
-            return await interaction.editReply("Your rank card background has been reset!");
-        }
-        var valid = /^(http|https):\/\/[^ "]+$/.test(interaction.options.getString("bgurl")) && (interaction.options.getString("bgurl").endsWith(".png") || interaction.options.getString("bgurl").endsWith(".jpg") || interaction.options.getString("bgurl").endsWith("jpeg"));
-        if (!valid) return await interaction.editReply("Please enter a valid image URL. It must use HTTP or HTTPS and be a PNG, JPG, or JPEG image. It must not have a `?` or `&` at the end.");
+	name: "background",
+	aliases: ["bg"],
+	description: "Set a card background",
+	cooldown: 3,
+	options: [
+		{
+			name: "bgurl",
+			description: "The image URL for the background you wish to add (leave empty to reset)",
+			type: 3,
+			required: false,
+		},
+	],
+	category: "Leveling",
+	async execute(interaction) {
+		await interaction.deferReply();
+		const background = interaction.options.getString("bgurl");
 
-        try {
-            sql.prepare("INSERT OR REPLACE INTO background (user, guild, bg) VALUES (?, ?, ?);").run(interaction.member.id, interaction.guild.id, interaction.options.getString("bgurl"));
-            return await interaction.editReply("Background set!");
-        } catch (e) {
-            return await interaction.editReply("An error occurred while setting the background.");
-            console.error(e);
-        }
-    }
-}
+		if (!background) {
+			sql.prepare("DELETE FROM background WHERE user = (?);").run(interaction.user.id);
+			return interaction.followUp("Your rank card background has been reset!");
+		}
+		var valid =
+			/^(http|https):\/\/[^ "]+$/.test(background) &&
+			(background.endsWith(".png") || background.endsWith(".jpg") || background.endsWith("jpeg"));
+		if (!valid)
+			return interaction.followUp(
+				"Please enter a valid image URL. It must use HTTP or HTTPS and be a PNG, JPG, or JPEG image. It must not have a `?` or `&` at the end."
+			);
+
+		try {
+			sql
+				.prepare("INSERT OR REPLACE INTO background (user, guild, bg) VALUES (?, ?, ?);")
+				.run(interaction.member.id, interaction.guild.id, interaction.options.getString("bgurl"));
+			return interaction.followUp("Background set!");
+		} catch (e) {
+			console.error(e);
+			return interaction.followUp("An error occurred while setting the background.");
+		}
+	},
+};
