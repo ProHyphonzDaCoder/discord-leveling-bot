54a7c0f2ed5d312ca92b67b12600813f7aae1cd1
DaanGamesDG <daan@daangamesdg.wtf>
2021-08-24 20:10:17 +0200

    Fix(interactionCreate): interaction deferred error

--- index.js
+++ index.js
@@ -1,78 +1,79 @@
 // Importing Packages
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const client = new Discord.Client({
-    intents: [
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_MEMBERS,
-        Discord.Intents.FLAGS.GUILD_PRESENCES
-    ]
+	intents: [
+		Discord.Intents.FLAGS.GUILDS,
+		Discord.Intents.FLAGS.GUILD_MESSAGES,
+		Discord.Intents.FLAGS.GUILD_MEMBERS,
+		Discord.Intents.FLAGS.GUILD_PRESENCES,
+	],
 });
 client.commands = new Discord.Collection();
 
 const SQLite = require("better-sqlite3");
-const sql = new SQLite('./mainDB.sqlite');
+const sql = new SQLite("./mainDB.sqlite");
 const { join } = require("path");
 const fs = require("fs");
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./commands").filter((file) => file.endsWith(".js"));
+const eventFiles = fs.readdirSync("./events").filter((file) => file.endsWith(".js"));
 
 // Token, Owner ID, and Application ID
 const config = require("./config.json");
 const sqlFunctions = require("./sql_functions/sql_functions");
 
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    // set a new item in the Collection
-    // with the key as the command name and the value as the exported module
-    client.commands.set(command.name, command);
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
 }
 
 for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (file.once) {
-        client.once(event.name, event.execute);
-    } else {
-        client.on(event.name, event.execute);
-    }
+	const event = require(`./events/${file}`);
+	if (file.once) {
+		client.once(event.name, event.execute);
+	} else {
+		client.on(event.name, event.execute);
+	}
 }
 
-const commands = client.commands.map(({ execute, ...data }) => data); 
+const commands = client.commands.map(({ execute, ...data }) => data);
 
-const rest = new REST({ version: '9' }).setToken(config.token);
+const rest = new REST({ version: "9" }).setToken(config.token);
 
 (async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
+	try {
+		console.log("Started refreshing application (/) commands.");
 
-        await rest.put(
-            Routes.applicationCommands(config.appID),
-            { body: commands },
-        );
+		await rest.put(Routes.applicationCommands(config.appID), { body: commands });
 
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-      console.error(error);
-    }
+		console.log("Successfully reloaded application (/) commands.");
+	} catch (error) {
+		console.error(error);
+	}
 })();
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+	if (!interaction.isCommand()) return;
+	if (!client.commands.has(interaction.commandName)) return;
 
-    if (!client.commands.has(interaction.commandName)) return;
-
-    try {
-        await client.commands.get(interaction.commandName).execute(interaction);
-    } catch (error) {
-        console.error(error);
-        return interaction.reply({
-            content: 'There was an error while executing this command!',
-            ephemeral: true
-        });
-    }
+	try {
+		await client.commands.get(interaction.commandName).execute(interaction);
+	} catch (error) {
+		console.error(error);
+		interaction.replied || interaction.deferred
+			? interaction.followUp({
+					content: "There was an error while executing this command!",
+					ephemeral: true,
+			  })
+			: interaction.reply({
+					content: "There was an error while executing this command!",
+					ephemeral: true,
+			  });
+	}
 });
 
 client.login(config.token);
