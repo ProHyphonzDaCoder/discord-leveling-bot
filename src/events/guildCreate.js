747a5601d6d94c43ee17df623b2e822ff804e006
DaanGamesDG <daan@daangamesdg.wtf>
2021-09-14 22:58:11 +0200

    refactor: switching events to classes

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,17 +1,21 @@
+const EventListener = require("../structures/EventListener");
 const { sql, deleteLevel } = require("./../functions/sql");
 
 const getLevels = sql.prepare("SELECT user FROM levels WHERE guild = ?");
 
-module.exports = {
-	name: "guildCreate",
-	execute: async (guild) => {
+module.exports = class GuildCreate extends EventListener {
+	constructor(context) {
+		super(context, {
+			name: "guildCreate",
+		});
+	}
 
+	run(guild) {
 		const storedLevels = getLevels.all(guild.id);
 		const storedUserIDs = storedLevels.map((level) => level.user);
 
 		const memberIDs = guild.members.cache.map((m) => m.id);
 		const unknownIDs = storedUserIDs.filter((userID) => !memberIDs.includes(userID));
 		unknownIDs.forEach((userID) => deleteLevel.run(userID, guild.id));
-
-	},
+	}
 };
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,17 @@
-const fs = require("fs");
-
+const EventListener = require("../structures/EventListener");
 const { sql, deleteLevel } = require("./../functions/sql");
 const getLevels = sql.prepare("SELECT user FROM levels WHERE guild = ?");
 
-module.exports = {
-	name: "ready",
-	execute: (client) => {
-		client.guilds.cache.each((guild) => {
+module.exports = class Ready extends EventListener {
+	constructor(context) {
+		super(context, {
+			name: "ready",
+			once: true,
+		});
+	}
+
+	run() {
+		this.client.guilds.cache.each((guild) => {
 			const storedLevels = getLevels.all(guild.id);
 			const storedUserIDs = storedLevels.map((level) => level.user);
 
@@ -15,23 +20,14 @@ module.exports = {
 			unknownIDs.forEach((userID) => deleteLevel.run(userID, guild.id));
 		});
 
-		console.log(`Logged in as ${client.user.tag}!`);
+		console.log(`Logged in as ${this.client.user.tag}!`);
 
-		const data = [];
-		const commandFiles = fs
-			.readdirSync(`${__dirname}/../commands`)
-			.filter((file) => file.endsWith(".js"));
-		for (const file of commandFiles) {
-			const object = {};
-			const command = require(`./../commands/${file}`);
+		const commands = this.client.commands.map((cmd) => ({
+			name: cmd.name,
+			description: cmd.description,
+			options: cmd.options,
+		}));
 
-			if (command.name) object.name = command.name;
-			if (command.description) object.description = command.description;
-			if (command.options) object.options = command.options;
-
-			data.push(object);
-		}
-
-		client.application.commands.set(data);
-	},
+		this.client.application.commands.set(commands);
+	}
 };
