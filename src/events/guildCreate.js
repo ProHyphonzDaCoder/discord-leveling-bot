43e891c469b311a85c375396cd4cfd978f219aa2
m8eee <aldous0601@yahoo.com>
2021-09-18 07:18:38 +0800

    Message Upon Entering Server

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,7 +1,13 @@
 const EventListener = require("../structures/EventListener");
+const config = require("../../config.json");
 const { sql, deleteLevel } = require("./../functions/sql");
 
 const getLevels = sql.prepare("SELECT user FROM levels WHERE guild = ?");
+
+const greetText = `**Thank you for adding Hyphonz**
+
+- Use /help to see my commands
+- If you need more help, please visit my [support server](https://discord.gg/6SbwSCzehm)`
 
 module.exports = class GuildCreate extends EventListener {
 	constructor(context) {
@@ -17,5 +23,12 @@ module.exports = class GuildCreate extends EventListener {
 		const memberIDs = guild.members.cache.map((m) => m.id);
 		const unknownIDs = storedUserIDs.filter((userID) => !memberIDs.includes(userID));
 		unknownIDs.forEach((userID) => deleteLevel.run(userID, guild.id));
+
+		const greetChannel = guild.channels.cache.find(channel => {
+			return channel.isText() && channel.permissionsFor(config.appID).has("SEND_MESSAGES")
+		});
+		if (greetChannel) {
+			greetChannel.send(greetText);
+		}
 	}
 };
